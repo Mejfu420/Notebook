ed137904af07b1354f37f1693ed5b6b8f02def9f
mateusz.nitkowski@gmail.com <Jungkookopp@123>
2026-06-01 23:32:36 +0200

    fix: stacja 2 jezus bierze krzyz na swe ramiona

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,7 @@
 import app from './app';
 
-const PORT = process.env.PORT || 8080;
+const PORT = parseInt(process.env.PORT || '8080', 10);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
